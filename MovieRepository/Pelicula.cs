8e1730363748aac035d63dc09c041fb5367c9f1e
Edgar1Gallegos <66189461+Edgar1Gallegos@users.noreply.github.com>
2023-06-12 12:00:53 -0500

    Add files via upload

---
+++ MovieRepository/Pelicula.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Pelicula
+{
+
+	private string Nombre;
+	private int Anio;
+
+	public Pelicula(string Nombre, int Anio)
+	{
+		this.Nombre = nombre;
+		this.Anio = anio;
+	}
+
+	public string Nombre
+	{
+		get { return Nombre; }
+		set { Nombre = value; }
+	}
+
+	public int Anio
+	{
+        get { return Anio; }
+        set { Aombre = value; }
+    }
+}
